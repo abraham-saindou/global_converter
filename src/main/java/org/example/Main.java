8cba6c8db6bf96e79d1e14c3c7e1cf0ee812b0e1
Abraham-Saindou <abraham.saindou@laplateforme.io>
2023-05-29 19:02:41 +0200

    working on project

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,11 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     String spe_character = "!$%&?@&#'{([-|`_^]}=£*";
     public static void main(String[] args) {
-        checkword();
+        convert_to_ascii();
     }
     public static List<Character> mkword(){
         Scanner input = new Scanner(System.in);
@@ -23,33 +21,43 @@ public class Main {
         return charList;
     }
 
-    public static String checkword(){
+    public static List<Character> checkword(){
         boolean arrow = false;
         Main myObj = new Main();
-        String wordtoconvert = mkword();
-        for (int i = 0; i < wordtoconvert.length(); ++i){
-            for (int j = 0; j < myObj.spe_character.length(); ++j){
-                if (wordtoconvert.charAt(i) == myObj.spe_character.charAt(j)){
+        List<Character> wordtoconvert = mkword();
+
+        for (Character character : wordtoconvert) {
+            for (int j = 0; j < myObj.spe_character.length(); ++j) {
+                if (character == myObj.spe_character.charAt(j)) {
                     System.out.println("This character " + myObj.spe_character.charAt(j) + " is in your word. Try a new word");
                     mkword();
-                }
-                else {
+                } else {
                     System.out.println("Your word to analyze is good.");
                     arrow = true;
                     break;
                 }
             }
-            if (arrow){
+            if (arrow) {
                 break;
             }
-
         }
         return wordtoconvert;
     }
-    public static void ask_conversion(){
+    public static Map<Character, Integer> convert_to_ascii(){
+        List<Character> list = checkword();
+
         Scanner input = new Scanner(System.in);
         System.out.println("Which base do you want to convert your word into ?");
         System.out.println("a for octal, b for hexadecimal, c for binary and d for text");
         String choice = input.next();
+
+        Map<Character, Integer> map1 = new LinkedHashMap<>();
+
+        for (int i = 0; i < list.size(); ++i){
+            int ascii = list.get(i);
+            map1.put(list.get(i), ascii);
+        }
+        System.out.println(map1);
+        return map1;
     }
 }
